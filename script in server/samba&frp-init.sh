b7a4915b48e1bb7821fa58d3f00c114464e182ba
xiaoxx <me@xiaoxx.cc>
2019-10-28 23:24:19 +0800

    update

--- script in server/samba&frp-init.sh
+++ script in server/samba&frp-init.sh
@@ -28,13 +28,14 @@ echo '\n' | testparm
 echo "samba服务配置完成，请设定密码："
 sudo smbpasswd -a ljp
 
-FRP_VER=v0.28.2
+FRP_VER=0.29.0
 echo "开始安装frp"
 cd ~
-wget https://github.com/fatedier/frp/releases/download/$FRP_VER/frp_$FRP_VER_linux_amd64.tar.gz
-tar -xvf frp_$FRP_VER_linux_amd64.tar.gz && rm frp_$FRP_VER_linux_amd64.tar.gz
+wget https://github.com/fatedier/frp/releases/download/v${FRP_VER}/frp_${FRP_VER}_linux_amd64.tar.gz
+tar -xvf frp_{$FRP_VER}_linux_amd64.tar.gz && \
+rm -f frp_{$FRP_VER}_linux_amd64.tar.gz
 sudo mkdir /etc/frp/ /usr/local/bin/frp/
-sudo mv frp_$FRP_VER_linux_amd64/frpc /usr/local/bin/frp/
+sudo mv frp_${FRP_VER}_linux_amd64/frpc /usr/local/bin/frp/
 sudo tee /etc/frp/frpc.ini <<-'EOF'
 [common]
 server_addr = ali.mcyo.pw
